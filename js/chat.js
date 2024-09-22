e662bdd6c4241ce4115b67df23a77d16c8cfb804
amliyanage <ashenmliyanage@gmail.com>
2024-09-22 12:44:07 +0530

    chart popup developed

---
+++ js/chat.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    const ctx = document.getElementById('myPieChart').getContext('2d');
+    const myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow'],
+            datasets: [{
+                label: 'My First Dataset',
+                data: [300, 50, 100],
+                backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+                hoverOffset: 4
+            }]
+        },
+        options: {
+            responsive: true,
+        }
+    });
+});
